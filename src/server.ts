2b2779d31547b140a9b2f9ce3c0052765c279d23
Guilherme Pereira da Silva <guilherme.pereiradasilva-ext@ab-inbev.com>
2023-11-05 01:35:23 -0300

    vercel config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from "./app";
+import dotenv from 'dotenv'; 
+dotenv.config();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server running at 3000");
 });
